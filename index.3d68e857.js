adafb3806c18581ca1e1f875b34eac33f5b4eded
Alina Kabanets <kabanets.dolls@gmail.com>
2024-01-08 14:50:04 +0000

    make build

---
+++ index.3d68e857.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider");e.style.top="".concat(t.clientWidth/2-e.offsetWidth/2,"px"),e.style.left="".concat(t.clientHeight/2-e.offsetWidth/2,"px");
+//# sourceMappingURL=index.3d68e857.js.map
